68f0d7b935d2e5fdac74644550955fe11eb25e3a
Harun Sokullu <harunsokullu@gmail.com>
2020-08-13 23:12:48 +0300

    update useragent

--- src/helpers/constant.ts
+++ src/helpers/constant.ts
@@ -1,5 +1,5 @@
 export const Constants = {
-  UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+  UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
   LaunchOptions: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
