249bdfb6fbea518047fcdb6732e39789cb10fbd8
Harun Sokullu <harunsokullu@gmail.com>
2020-08-14 12:50:44 +0300

    wait for 2 factor

--- src/controllers/puppet/steam.ts
+++ src/controllers/puppet/steam.ts
@@ -8,9 +8,7 @@ async function login(page: puppeteer.Page, steamLogin: ISteamLogin) {
   await page.type('#steamPassword', steamLogin.password);
   if (config.SAVE_SCREENSHOT) { await page.screenshot({ fullPage: true, path: 'steamInitial.jpg' }); }
   await page.click('#imageLogin');
-
-  // TODO: handle
-  await helpers.sleep(2000);
+  await page.waitForSelector('#twofactorcode_entry')
 
   await page.type('#twofactorcode_entry', steamLogin.twoFactorCode);
   if (config.SAVE_SCREENSHOT) { await page.screenshot({ fullPage: true, path: 'steamTwoFactor.jpg' }); }
