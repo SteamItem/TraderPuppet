58e8f714284aefad06e874d37e3caf7da246c54e
Harun Sokullu <harunsokullu@gmail.com>
2020-09-01 12:50:32 +0300

    steam login wait selector

--- src/controllers/puppet/steam.ts
+++ src/controllers/puppet/steam.ts
@@ -12,8 +12,8 @@ async function login(page: puppeteer.Page, steamLogin: ISteamLogin) {
   await helpers.screenshot(page, 'steamInitial.jpg');
   await page.click('#imageLogin');
 
-  await helpers.sleep(7000);
-  // await page.waitForSelector('.loginTwoFactorCodeModal', { visible: true })
+  // await helpers.sleep(7000);
+  await page.waitForSelector('.loginTwoFactorCodeModal', { visible: true })
   await page.type('#twofactorcode_entry', steamLogin.twoFactorCode);
   await helpers.screenshot(page, 'steamTwoFactor.jpg');
   await page.keyboard.press('Enter');
