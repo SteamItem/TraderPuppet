6137f4e791ab55fe444e42b6b502b1f2dbdc2217
Harun Sokullu <harunsokullu@gmail.com>
2020-09-03 22:48:31 +0300

    update rollbit login

--- src/controllers/puppet/rollbit.ts
+++ src/controllers/puppet/rollbit.ts
@@ -3,7 +3,7 @@ import steam from './steam';
 import { ISteamLogin } from '../../interfaces/steam';
 import { Constants } from '../../helpers/constant';
 
-async function login(steamLogin: ISteamLogin) {
+async function login_old(steamLogin: ISteamLogin) {
   const browser = await helpers.launchBrowser();
   try {
     const mainPage = await browser.newPage();
@@ -16,8 +16,39 @@ async function login(steamLogin: ISteamLogin) {
     const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
     const steamLoginPage = await newTarget.page();
 
-    await steam.login(steamLoginPage, steamLogin);
+    await steam.login_old(steamLoginPage, steamLogin);
     while (!steamLoginPage.isClosed) { await helpers.sleep(50); }
+    await mainPage.waitForSelector('.uppercase.relative.active')
+
+    await helpers.screenshot(mainPage, 'rollbitLogin.jpg');
+    const cookies = await mainPage.cookies();
+    return cookies;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
+    await browser.close();
+  }
+}
+
+async function login(steamLogin: ISteamLogin) {
+  const browser = await helpers.launchBrowser();
+  try {
+    const steamPage = await browser.newPage();
+    await steam.login(steamPage, steamLogin);
+
+    const mainPage = await browser.newPage();
+    await mainPage.setUserAgent(Constants.UserAgent);
+    await mainPage.goto('https://www.rollbit.com');
+    await mainPage.waitForSelector('.bg-green')
+    await mainPage.click('.bg-green');
+
+    const pageTarget = mainPage.target();
+    const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
+    const steamUserSelectPage = await newTarget.page();
+
+    await steam.selectUser(steamUserSelectPage);
+    while (!steamUserSelectPage.isClosed) { await helpers.sleep(50); }
     await mainPage.waitForSelector('.uppercase.relative.active')
 
     await helpers.screenshot(mainPage, 'rollbitLogin.jpg');
--- src/controllers/puppet/steam.ts
+++ src/controllers/puppet/steam.ts
@@ -3,7 +3,7 @@ import helpers from '../../helpers';
 import { ISteamLogin } from '../../interfaces/steam';
 import { Constants } from '../../helpers/constant';
 
-async function login(page: puppeteer.Page, steamLogin: ISteamLogin) {
+async function login_old(page: puppeteer.Page, steamLogin: ISteamLogin) {
   await page.setUserAgent(Constants.UserAgent);
 
   await page.waitForSelector('#imageLogin');
@@ -19,6 +19,29 @@ async function login(page: puppeteer.Page, steamLogin: ISteamLogin) {
   await page.keyboard.press('Enter');
 }
 
+async function login(page: puppeteer.Page, steamLogin: ISteamLogin) {
+  await page.setUserAgent(Constants.UserAgent);
+  await page.goto('https://store.steampowered.com/login');
+  await page.waitForSelector('#login_btn_signin');
+
+  await page.type('#input_username', steamLogin.username);
+  await page.type('#input_password', steamLogin.password);
+  await page.keyboard.press('Enter');
+
+  await page.waitForSelector('#twofactorcode_entry', { visible: true })
+  await page.type('#twofactorcode_entry', steamLogin.twoFactorCode);
+  await page.keyboard.press('Enter');
+  await page.waitForNavigation();
+}
+
+async function selectUser(page: puppeteer.Page) {
+  await page.setUserAgent(Constants.UserAgent);
+  await page.waitForSelector('#imageLogin');
+  await page.click('#imageLogin');
+}
+
 export default {
-  login
+  login,
+  login_old,
+  selectUser
 }
