732fb9a3478d777647cbfdbb872f0d5bd11e4025
Harun Sokullu <harunsokullu@gmail.com>
2020-09-28 21:08:27 +0300

    update eco args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,6 @@ module.exports = {
   apps : [{
     name: "server",
     script: "./dist/server.js",
-    time: true,
-    env: {
-      NODE_ENV: "production",
-      SAVE_SCREENSHOT: "true",
-      HEADLESS: "true"
-    }
+    time: true
   }]
 }
